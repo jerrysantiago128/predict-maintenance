0957edee51f99a055051bd1efe8696c44a195520
Jerry Santiago <jerrysantiago128@gmail.com>
2025-06-02 13:41:05 -0400

    commenting out print statements for cleaning terminal output

--- prototypes/main.py
+++ prototypes/main.py
@@ -4,7 +4,7 @@ import vibration_sensor
 
 if __name__ == "__main__":
     temperature_json_data = temperature_sensor.generate_temperature_data()
-    print("Temperature Data:\n", temperature_json_data)
+    #print("Temperature Data:\n", temperature_json_data)
 
     # Optional: Save to a file
     with open("temperature_data.json", "w") as f:
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
 
     battery_json_data = battery_sensor.generate_battery_data()
-    print("\nBattery Data:\n", battery_json_data)
+    #print("\nBattery Data:\n", battery_json_data)
 
     # Optional: Save to a file
     with open("battery_data.json", "w") as f:
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
 
     vibration_json_data = vibration_sensor.generate_vibration_data()
-    print("\nVibration Data:\n", vibration_json_data)
+    #print("\nVibration Data:\n", vibration_json_data)
 
     # Optional: Save to a file
     with open("vibration_data.json", "w") as f:
